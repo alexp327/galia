d9331eedb5dac4968a16b47c8147d720fb47105a
Alex Perry <alex.perry422@gmail.com>
2023-10-23 17:00:00 -0400

    get all reviews and create new review

--- backend-galia/src/review/controllers/review.controller.ts
+++ backend-galia/src/review/controllers/review.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Review } from '../models/review.interface';
+import { ReviewService } from '../services/review.service';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+  constructor(private reviewService: ReviewService) {}
+
+  @Get()
+  getAllReviews(): Observable<Review[]> {
+    return this.reviewService.findAllReviews();
+  }
+
+  @Post()
+  createReview(@Body() review: Review): Observable<Review> {
+    return this.reviewService.createReview(review);
+  }
+}
--- backend-galia/src/review/services/review.service.ts
+++ backend-galia/src/review/services/review.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CompletedReviewEntity } from '../models/review.entity';
+import { Repository } from 'typeorm';
+import { Review } from '../models/review.interface';
+import { Observable, from } from 'rxjs';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(
+    @InjectRepository(CompletedReviewEntity)
+    private readonly reviewRepository: Repository<CompletedReviewEntity>,
+  ) {}
+
+  findAllReviews(): Observable<Review[]> {
+    return from(this.reviewRepository.find());
+  }
+
+  createReview(review: Review): Observable<Review> {
+    return from(this.reviewRepository.save(review));
+  }
+}
