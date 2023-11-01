b22dc0a3e1c5d8fce8b39352bc160c4b59c2e89b
Alex Perry <alex.perry422@gmail.com>
2023-11-01 17:40:39 -0400

    add shadcn and lucide-react, remove daisyui

--- frontend-galia/app/layout.tsx
+++ frontend-galia/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Link from 'next/link';
+import NavMenu from '@/components/navMenu';
+import { Button } from '@/components/ui/button';
+import { Twitter } from 'lucide-react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,79 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' data-theme='dark'>
+    <html lang='en'>
       <body className={inter.className}>
-        <div className='navbar bg-primary text-primary-content'>
-          <div className='navbar-start'>
-            <div className='dropdown'>
-              <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-5 w-5'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M4 6h16M4 12h8m-8 6h16'
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
-              >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className='p-2'>
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
-              </ul>
-            </div>
-            <Link href={"/"} className='btn btn-ghost normal-case text-xl'>Galia</Link>
-          </div>
-          <div className='navbar-center hidden lg:flex'>
-            <ul className='menu menu-horizontal px-1'>
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className='p-2'>
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <div className='navbar-end'>
-            <a className='btn'>Button</a>
-          </div>
-        </div>
+        <header className='flex justify-between max-w-6xl mx-auto px-4 py-3'>
+          <Button className='text-xl font-bold' variant='ghost'>
+            Galia
+          </Button>
+          <NavMenu />
+          <Button variant='ghost'>
+            <Twitter />
+          </Button>
+        </header>
         {children}
       </body>
     </html>
