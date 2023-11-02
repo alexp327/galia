3f460c1f75e189117ba8ea05ce6a8991fece55c2
Alex Perry <alex.perry422@gmail.com>
2023-11-01 23:32:35 -0400

    add menu

--- frontend-galia/app/layout.tsx
+++ frontend-galia/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavMenu from '@/components/navMenu';
 import { Button } from '@/components/ui/button';
-import { Twitter } from 'lucide-react';
+import { Plus, Twitter } from 'lucide-react';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,13 +22,24 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <header className='flex justify-between max-w-6xl mx-auto px-4 py-3'>
-          <Button className='text-xl font-bold' variant='ghost'>
-            Galia
-          </Button>
-          <NavMenu />
-          <Button variant='ghost'>
-            <Twitter />
-          </Button>
+          <div>
+            <div className='inline-block md:hidden mr-2'>hello</div>
+            <Button className='text-xl font-bold' variant='ghost'>
+              <Link href='/'>Galia</Link>
+            </Button>
+          </div>
+          <div className='hidden md:inline-block'>
+            <NavMenu />
+          </div>
+          <div className='flex gap-1'>
+            <Button variant='ghost' className='flex gap-2'>
+              <Plus />
+              <span className='hidden md:inline-block'>New Review</span>
+            </Button>
+            <Button variant='ghost'>
+              <Twitter />
+            </Button>
+          </div>
         </header>
         {children}
       </body>
